c6c2aa75a5d804017df98692d842eedf357ff2cd
Fer125432 <ferortells@hotmail.com>
2026-09-03 19:29:37 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2067571672"
+    serviceWorkerVersion: "2887179235"
   }
 });
